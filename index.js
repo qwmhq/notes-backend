ff050ffd64daf0690989337dc43763679e8a17e6
Quwam Ogunlaja <quwam32@gmail.com>
2023-08-07 16:41:00 +0100

    modified / endpoint

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ let notes = [
     { id: 3, content: "GET and POST are the most important methods of HTTP protocol", important: true }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
